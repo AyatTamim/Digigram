7343c7f7fd505f0376e5226197e4003f959b57df
AyatTamim <126102467+AyatTamim@users.noreply.github.com>
2023-04-25 14:30:02 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// var reviewChild = document.getElementsByClassName("tesT");
+// console.log(reviewChild);
+
+
+// for (let index = 0; index < reviewChild.length; index++) {
+//     const child = reviewChild[index];
+//     for (let index = 0; index < reviewChild.length; index++) {
+//         const child = reviewChild[index];
+//         child.addEventListener("click", function () {
+//             for (let index = 0; index < child.length; index++) {
+//                 const element = child[index];
+//                 element.classList.remove("slider");
+//             }
+//             this.classList.toggle("slider")
+//             })
+//     };
+
+// };
